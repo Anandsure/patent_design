3d18b15e7d227614aed48b8d93f514c1d1846494
Anand Suresh <anandsure@Anands-MacBook-Pro.local>
2023-10-10 12:21:28 +0530

    initialised db on run

--- api/db/db.go
+++ api/db/db.go
@@ -6,10 +6,18 @@ import (
 	"log"
 	"time"
 
-	"github.com/spf13/viper"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
+)
+
+const (
+	host      = "localhost"
+	port      = 5432
+	user      = "anands"
+	password  = "87szLCJM"
+	dbname    = "design_patent"
+	tableName = "patents"
 )
 
 var db *gorm.DB = nil
@@ -24,11 +32,7 @@ func GetDB() *gorm.DB {
 }
 
 func Connect() *gorm.DB {
-	username := viper.GetString("DB_USER")
-	password := viper.GetString("DB_PASS")
-	dbName := viper.GetString("DB_NAME")
-	dbHost := viper.GetString("DB_HOST")
-	dbUri := fmt.Sprintf("host=%s user=%s dbname=%s port=5432 sslmode=disable password=%s", dbHost, username, dbName, password)
+	dbUri := fmt.Sprintf("host=%s user=%s dbname=%s port=%d sslmode=disable password=%s", host, user, dbname, port, password)
 	sqlDB, err := sql.Open("postgres", dbUri)
 	if err != nil {
 		log.Fatal(err)
